3d4c0fe47661bf7d55079820bbeb301573f9a3de
404pandas <mary.panda.jackson@gmail.com>
2025-10-10 13:37:03 -0400

    Added quick action

--- witcherssatchel/app/_layout.tsx
+++ witcherssatchel/app/_layout.tsx
@@ -1,6 +1,21 @@
 import { Stack } from "expo-router";
+import * as QuickActions from "expo-quick-actions";
+import { useEffect } from "react";
+import { Platform } from "react-native";
+import { useQuickActionRouting } from "expo-quick-actions/router";
 
 export default function Layout() {
+  useQuickActionRouting();
+  useEffect(() => {
+    QuickActions.setItems([
+      {
+        title: "Add a potion",
+        icon: Platform.OS === "ios" ? "symbol:flask" : "potion",
+        id: "0",
+        params: { href: "/new" },
+      },
+    ]);
+  }, []);
   return (
     <Stack>
       <Stack.Screen
